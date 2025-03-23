8db128a25720d2ac27a0da2a31e6fca2839ce770
ChrisnNg <cristopherng@hotmail.com>
2025-03-22 18:36:25 -0700

    programically set environment to serve outputs dynamically

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,25 @@ const withMDX = mdx({
   options: {},
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-  // output: "export", // <=== enables static exports
-  reactStrictMode: true,
-};
+const env = process.env.NODE_ENV;
+let nextConfig;
+
+if (env === "development") {
+  /* development only config options here */
+  /** @type {import('next').NextConfig} */
+  nextConfig = {
+    pageExtensions: ["ts", "tsx", "md", "mdx"],
+    reactStrictMode: true,
+  };
+} else {
+  /* config options for all phases except development here */
+
+  /** @type {import('next').NextConfig} */
+  nextConfig = {
+    pageExtensions: ["ts", "tsx", "md", "mdx"],
+    output: "export", // <=== enables static exports (required for github pages)
+    reactStrictMode: true,
+  };
+}
 
 export default withMDX(nextConfig);
