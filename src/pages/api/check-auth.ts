1a5dd75569eadf71c371055477f46666ae02eae4
Dyad <cristopherng@hotmail.com>
2026-09-04 12:56:05 -0700

    (2 files changed)

--- src/pages/api/check-auth.ts
+++ src/pages/api/check-auth.ts
@@ -1,12 +1,5 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import * as cookie from "cookie";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//   const cookies = cookie.parse(req.headers.cookie || "");
-
-//   if (cookies.authToken === "authenticated") {
-//     return res.status(200).json({ authenticated: true });
-//   } else {
-//     return res.status(401).json({ authenticated: false });
-//   }
-// }
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({ authenticated: true });
+}
